0ed20fcd190028f989ccde666c9b41edb350a099
Ethan Ruffin <150500664+GranularHyphen@users.noreply.github.com>
2026-01-13 18:50:06 -0500

    Add files via upload

---
+++ trigonometry-libarary/src/main.c
@@ -0,0 +1,28 @@
+//use libarary in a script
+
+#include "geometry.h"
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Do you want to:\n");
+    printf(" 1. Find sector length\n");
+    printf(" 2. Find sector area\n");
+    printf(" 3. Find distance between two points\n");
+    printf("Enter choice (1,2, 3): ");
+
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        sector_length();
+    } else if (choice == 2) {
+        sector_area();
+    } else if (choice == 3) {
+        distance();
+    } else {
+        printf("Invalid choice.\n");
+    }
+
+    return 0;
+}
